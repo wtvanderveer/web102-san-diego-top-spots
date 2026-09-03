deecfca5ae755cea56600276955d8fa9a96d3317
william vanderveer <williamtvanderveer@gmail.com>
2026-09-03 15:10:29 -0700

    San Diego Top Spots

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-$(document).ready(function () {
-    // write your code here
+$.getJSON("data.json", function(data) {
+    const tbody = $(".dataTable tbody");
+    tbody.empty();
 
-});
+    $.each(data, function(index, item) {
+        const row = `
+        <tr>
+            <td>${item.name}</td>
+            <td>${item.description}</td>
+            <td><a href="https://www.google.com/maps?q=${item.location}" target=_blank class="map-btn">View in Google Maps<img src="Google_Maps_pin.svg.webp"alt="img of pin" width="9" height="16"></a></td>
+        </tr>
+        `;
+        tbody.append(row);
+    });
+})
+
+
